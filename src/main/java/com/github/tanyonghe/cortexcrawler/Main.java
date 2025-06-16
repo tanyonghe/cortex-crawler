fbf811647e292d7b003c206efce48f232c7bee61
tanyonghe <yonghe.tan@thoughtworks.com>
2025-06-16 18:52:47 +0800

    Refactor Main class to utilize domain allowlist and blocklist in WebCrawler initialization. Update initial URL setup for crawling with specified domains.

--- src/main/java/com/github/tanyonghe/cortexcrawler/Main.java
+++ src/main/java/com/github/tanyonghe/cortexcrawler/Main.java
@@ -1,13 +1,29 @@
 package com.github.tanyonghe.cortexcrawler;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.github.tanyonghe.cortexcrawler.crawler.WebCrawler;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        WebCrawler crawler = new WebCrawler(5, 2.0); // 5 threads, 2 requests/sec
-        crawler.addUrl("https://demo.cyotek.com", 3);
+        // Example domains to crawl
+        Set<String> allowedDomains = new HashSet<>();
+        allowedDomains.add("crawler-test.com");
+
+        // Example domains to block
+        Set<String> blockedDomains = new HashSet<>();
+        blockedDomains.add("blocked.com");
+        blockedDomains.add("malicious.org");
+
+        // Create crawler with 5 threads and 2 requests per second
+        WebCrawler crawler = new WebCrawler(5, 2.0, allowedDomains, blockedDomains);
+
+        // Add some initial URLs to crawl
+        crawler.addUrl("https://crawler-test.com/", 5);
+
         crawler.start();
     }
 }
